70f99785390d6092b19fa77f96bb3f06b4917c5b
Patricia Perez <patriciaperez@Patricias-MacBook-Air.local>
2018-10-17 18:17:40 -0400

    added database info

--- js/index.js
+++ js/index.js
@@ -4,3 +4,11 @@ import test from './script2.js';
 console.log('test');
 
 test();
+
+var database = firebase.database();
+var databaseRef = database.ref('/');
+
+databaseRef.once('value').then(function (snapshot) {
+    const databaseValue = snapshot.val();
+    console.log(databaseValue);
+});
